1125e2c208160b2e0b5c0d5ff8976a3df7c98bd1
walid <walid.sa3d@gmail.com>
2015-11-05 20:36:50 +0100

    refactor code

--- index.js
+++ index.js
@@ -4,57 +4,45 @@ var ipREGEX = require('ip-regex')
 var reREGEX = require('regex-regex')
 var fs = require('fs')
 
-var isFile = function (variable) {
-  return fs.lstatSync(variable).isFile()
-}
-var isURL = function (variable) {
-  return urlREGEX().test(variable)
-}
-var isString = function (variable) {
-  return typeof variable === 'string'
-}
-var isNumber = function (variable) {
-  return typeof variable === 'number'
-}
-var isInteger = function (variable) {
-  return isNumber(variable) && variable % 1 === 0
-}
-var isFloat = function (variable) {
-  return variable === isNumber(variable) && variable % 1 !== 0
-}
+module.exports = {
+  isFile: function (variable) {
+    return fs.lstatSync(variable).isFile()
+  },
+  isURL: function (variable) {
+    return urlREGEX().test(variable)
+  },
+  isString: function (variable) {
+    return typeof variable === 'string'
+  },
+  isNumber: function (variable) {
+    return typeof variable === 'number'
+  },
+  isInteger: function (variable) {
+    return this.isNumber(variable) && variable % 1 === 0
+  },
+  isFloat: function (variable) {
+    return variable === this.isNumber(variable) && variable % 1 !== 0
+  },
 
-var isLowerCase = function (variable) {
-  return variable === variable.toLowerCase()
+  isLowerCase: function (variable) {
+    return variable === variable.toLowerCase()
+  },
+  isUpperCase: function (variable) {
+    return variable === variable.toUpperCase()
+  },
+  isRegex: function (variable) {
+    return reREGEX.test(variable)
+  },
+  isDir: function (variable) {
+    return fs.lstatSync(variable).isDirectory()
+  },
+  isEmailAddress: function (variable) {
+    return emailREGEX().test(variable)
+  },
+  isIPAddress: function (variable) {
+    return ipREGEX().test(variable)
+  },
+  isArray: function (variable) {
+    return Array.isArray(variable)
+  }
 }
-var isUpperCase = function (variable) {
-  return variable === variable.toUpperCase()
-}
-var isRegex = function (variable) {
-  return reREGEX.test(variable)
-}
-var isDir = function (variable) {
-  return fs.lstatSync(variable).isDirectory()
-}
-var isEmailAddress = function (variable) {
-  return emailREGEX().test(variable)
-}
-var isIPAddress = function (variable) {
-  return ipREGEX().test(variable)
-}
-var isArray = function (variable) {
-  return Array.isArray(variable)
-}
-
-module.exports.isFile = isFile
-module.exports.isDir = isDir
-module.exports.isString = isString
-module.exports.isLowerCase = isLowerCase
-module.exports.isUpperCase = isUpperCase
-module.exports.isNumber = isNumber
-module.exports.isInteger = isInteger
-module.exports.isFloat = isFloat
-module.exports.isURL = isURL
-module.exports.isEmailAddress = isEmailAddress
-module.exports.isIPAddress = isIPAddress
-module.exports.isArray = isArray
-module.exports.isRegex = isRegex
